d8b999de3fc414fcd03233e788538625e5026b8b
lebowvsky <lebowvsky@gmail.com>
2020-11-06 10:53:58 +0100

    Add style

--- src/containers/wilder-container/wilders-container.jsx
+++ src/containers/wilder-container/wilders-container.jsx
@@ -1,29 +1,35 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 import Wilder from "../../components/wilder/wilder-cpnt";
 
 const WilderContainer = () => {
   const [wilders, setWilders] = useState([]);
 
-  useEffect(() => {
-    const fetchWilder = async () => {
-      try {
-        const result = await axios.get("/api/wilders");
-        const datas = result.data.result;
-        setWilders([...datas]);
-      } catch (err) {
-        console.log("erreur : ", err);
-      } finally {
-        console.log("fin!");
-      }
-    };
+  const fetchWilder = async () => {
+    try {
+      const result = await axios.get("/api/wilders");
+      const datas = result.data.result;
+      setWilders([...datas]);
+      console.log(wilders);
+    } catch (err) {
+      console.log("erreur : ", err);
+    } finally {
+      console.log("fin!");
+    }
+  };
 
+  useEffect(() => {
     fetchWilder();
   }, []);
+
+  const addWilderToState = () => {
+    fetchWilder();
+  }
+
   return (
     <article className="wilders-wrapper">
       {wilders.map((wilder) => (
-        <Wilder name={wilder.name} city={wilder.city} skills={wilder.skills} />
+        <Wilder name={wilder.name} city={wilder.city} skills={wilder.skills}/>
       ))}
     </article>
   );
